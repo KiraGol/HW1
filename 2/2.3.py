b15c623349cd6a384210005455fcbffb5046436b
KiraGol <kiragolovkova93@gmail.com>
2021-06-25 22:02:57 +0300

    third commit

---
+++ 2/2.3.py
@@ -0,0 +1,4 @@
+vegetarians = ["Tom Smith", "Jacob Carr", "Sharon French", "Charles Short"]
+omnivores = ["Gilbert Ramsey", "Robert Lane", "John Doyle", "Tom Smith"]
+vegetarians.extend(omnivores)
+print(vegetarians)
