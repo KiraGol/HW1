bfa63099bcbfd26183c1d570ca49e25c491f48df
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-06-28 23:41:21 +0300

    HW3: add reafactoring of structure and add comments
    * move modules from homework 1 into the homework_1 package
    
    * move modules from homework_2 into the homework_2 package
    
    * move modules from homework_3 into the homework_3 package
    
    * add examples of code to solve task 1 with foreach loop without for loop

--- 2/3.2.py
+++
@@ -1,5 +0,0 @@
-a = 2
-print(a * 2)
-print(a ** 2)
-print(a / 2)
-print(a // 2)
--- homework_3/3.3.py
+++ homework_3/3.3.py
@@ -4,7 +4,7 @@ enemies = ["John", "Johnatan", "Artur"]
 for friend in friends:
     if friend in enemies:
         print(friend + ', we are not the friends anymore')
-    elif friend == 'James':
+    elif friend == 'James':  # This pring is excess. Try to look on logical operators to cobine several conditions in one line.
         print()
     else:
         print(friend + ', we are the best friends')
