db04945f3e0cfed1d1e1e30d15f15ecd06482198
KiraGol <kiragolovkova93@gmail.com>
2021-07-09 21:56:38 +0300

    homework4

--- homework_4/4.2.py
+++ homework_4/4.2.py
@@ -2,3 +2,9 @@ friends = ["John", "Marta", "James", "Amanda", "Marianna"]
 print("Names".center(50,'*'))
 for name in friends:
     print(name.rjust(50))
+
+print("{:*^50}".format("Names"))
+friends2 = f"John\nMarta\nJames\nAmanda\nMarianna"
+for f in friends:
+    print("{:>50}".format(f))
+
