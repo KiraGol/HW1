a36e1b3c5cf2b9d9aeb53192f68926e3ac78620f
KiraGol <kiragolovkova93@gmail.com>
2021-07-18 17:56:10 +0300

    homework_6.2 changes

--- homework_6/6.2.py
+++ homework_6/6.2.py
@@ -1,4 +1,7 @@
-def square(side_of_square: int):
+from typing import Tuple, Union
+
+
+def square(side_of_square: int) -> Tuple[int, int, Union[int, float]]:
     p = 4 * side_of_square
     s = side_of_square ** 2
     d = side_of_square * (2 ** 0.5)
