44b62b3db7fa04cd922c64f13f4ca279a4d98e11
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-25 14:56:47 +0300

    HW7: update declaration of function

--- homework_7/7.3.py
+++ homework_7/7.3.py
@@ -1,8 +1,9 @@
 from typing import Union, Callable, List, Dict, Tuple
 
 
-def my_reduce(callback: Callable, sequence: Union[List, Tuple, Dict]) \
-        -> Union[List, Tuple, Dict]:
+def my_reduce(
+    callback: Callable, sequence: Union[List, Tuple, Dict]
+) -> Union[List, Tuple, Dict]:
     iterable_data = iter(sequence)
     result = next(iterable_data)
     for element in sequence:
