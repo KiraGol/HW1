ff6b76edab22ef1f2f5ff4ea4d9456216bd47dd1
KiraGol <kiragolovkova93@gmail.com>
2021-08-15 14:51:58 +0300

    homework_15.2

---
+++ homework_15/Market.py
@@ -0,0 +1,24 @@
+from homework_15.products import Apple, Banana, Celery, Strawberry
+from homework_15.products.Product import Product
+
+
+class Market:
+    @staticmethod
+    def get_product(name: str):
+        if name == "apple":
+            return Apple()
+        elif name == "banana":
+            return Banana()
+        elif name == "celery":
+            return Celery()
+        elif name == "strawberry":
+            return Strawberry()
+        else:
+            raise Exception("Incorrect name of product.")
+
+
+if __name__ == '__main__':
+    print(id(Market.get_product("apple")))
+    print(id(Market.get_product("banana")))
+    print(id(Market.get_product("celery")))
+    print(id(Market.get_product("strawberry")))
